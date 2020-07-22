cf3a907a490c3defb698d759d5896607e1640fbb
bakamoridesu <bakamoridesu@gmail.com>
2020-07-22 14:09:26 +0300

    improve resizable panels

--- src/components/common/ResizablePanels.js
+++ src/components/common/ResizablePanels.js
@@ -5,7 +5,7 @@ import Dots from "./Icons/Dots";
 class ResizablePanels extends Component {
   state = {
     isDragging: false,
-    right: window.innerWidth/2,
+    right: 50,
     resizerPos: null,
   }
 
@@ -15,10 +15,15 @@ class ResizablePanels extends Component {
     ReactDOM.findDOMNode(this).addEventListener('mouseleave', this.stopResize)
   }
 
+  percent () {
+    return window.innerWidth/100
+  }
+
   startResize = (event) => {
+
     this.setState({
       isDragging: true,
-      resizerPos: event.clientX,
+      resizerPos: event.clientX / this.percent(),
     })
   }
 
@@ -32,17 +37,18 @@ class ResizablePanels extends Component {
 
   resizePanel = (event) => {
     if (this.state.isDragging) {
-      console.log('clientX: ', event.clientX)
-      console.log('resizerPos: ', this.state.resizerPos)
-      const delta = event.clientX - this.state.resizerPos
-      console.log('delta: ', delta)
+      // counting in percents for better resizing experience
+      const percent = this.percent()
+      const delta = event.clientX/percent - this.state.resizerPos
+
+      // make borders for resizing (20% from left and right)
       const borderLeft = window.innerWidth*0.2
       const borderRight = window.innerWidth*0.8
       const betweenBorders = event.clientX > borderLeft && event.clientX < borderRight
       const movingFromBorder = (event.clientX > borderLeft && delta < 0) || (event.clientX < borderRight && delta > 0)
       if(betweenBorders || movingFromBorder) {
         this.setState(({right, resizerPos}) => ({
-          right: right - delta,
+          right: right - delta ,
           resizerPos: resizerPos + delta
         }))
       }
@@ -50,17 +56,16 @@ class ResizablePanels extends Component {
   }
 
   render() {
-    const rest = this.props.children.slice(1)
     return (
       <div className="res_panel_container" onMouseUp={() => this.stopResize()}>
-        <div className="res_panel" style={{width: `calc(100% - ${this.state.right}px`}}>
+        <div className="res_panel" style={{width: `calc(100% - ${this.state.right}%`}}>
           {this.props.children[0]}
         </div>
         <div onMouseDown={(e) => this.startResize(e)}
              className="resizer" >
           <Dots/>
         </div>
-        <div className="res_panel" style={{width: `${this.state.right}px`}}>
+        <div className="res_panel" style={{width: `${this.state.right}%`}}>
           {this.props.children[1]}
         </div>
       </div>
